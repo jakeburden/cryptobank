600a5836389763f102da92350908849a6a650257
Jake Burden <jake@doge.haus>
2017-12-08 01:16:09 -0500

    encrypt whole log (problem 11)

--- bank.js
+++ bank.js
@@ -3,17 +3,24 @@ var net = require('net')
 var fs = require('fs')
 var jsonStream = require('duplex-json-stream')
 var sodium = require('sodium-native')
+var parallel = require('run-parallel')
 
-var log, keypair
-try {
-  log = require('./data.json')
-} catch (e) {
-  log = []
-}
+var log, keypair, box
 
 try {
   keypair = require('./keypair.json')
+  box = require('./box.json')
+  data = require('./data.json')
+
+  var cipher = Buffer.from(data.cipher, 'hex')
+  var nonce = Buffer.from(data.nonce, 'hex')
+  var boxSecretKey = Buffer.from(box.secretKey, 'hex')
+
+  var plainText = Buffer.alloc(cipher.length - sodium.crypto_secretbox_MACBYTES)
+  sodium.crypto_secretbox_open_easy(plainText, cipher, nonce, boxSecretKey)
+  log = JSON.parse(plainText.toString())
 } catch (e) {
+  console.log('creating keys...')
   var publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
   var secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
   sodium.crypto_sign_keypair(publicKey, secretKey)
@@ -26,6 +33,38 @@ try {
   fs.writeFile('keypair.json', JSON.stringify(keypair, null, 2), function (err) {
     if (err) console.error(err)
   })
+
+  var boxSecretKey = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(boxSecretKey)
+
+  box = {
+    secretKey: boxSecretKey.toString('hex')
+  }
+
+  fs.writeFile('box.json', JSON.stringify(box, null, 2), function (err) {
+    if (err) console.error(err)
+  })
+
+  var writeKeys = [
+    function (cb) {
+      write('keypair.json', keypair, function (err) {
+        if (err) return cb(new Error('Could not write keypair.'))
+        cb()
+      })
+    },
+    function (cb) {
+      write('box.json', box, function (err) {
+        if (err) return cb(new Error('Could not write box secretKey.'))
+        cb()
+      })
+    }
+  ]
+
+  parallel(writeKeys, function (err) {
+    if (err) return new Error(err)
+    console.log('keys created!')
+  })
+  log = []
 }
 
 var commands = {
@@ -42,18 +81,23 @@ commands.balance = commands.genesis = commands.withdraw
 var secretKey = Buffer.from(keypair.secretKey, 'hex')
 var publicKey = Buffer.from(keypair.publicKey, 'hex')
 
-var genesisEntry = {
-  command: 'genesis',
-  amount: 0
-}
+var boxSecretKey = Buffer.from(box.secretKey, 'hex')
 
-var genesisObj = {
-  value: genesisEntry,
-  hash: hashToHex('genesis hash'),
-  signature: sigToHex(Buffer.from(JSON.stringify(genesisEntry)), secretKey)
-}
+// add a genesis block to new logs
+if (!log.length) {
+  var genesisEntry = {
+    command: 'genesis',
+    amount: 0
+  }
 
-log.push(genesisObj)
+  var genesisObj = {
+    value: genesisEntry,
+    hash: hashToHex('genesis hash'),
+    signature: sigToHex(Buffer.from(JSON.stringify(genesisEntry)), secretKey)
+  }
+
+  log.push(genesisObj)
+}
 
 var server = net.createServer(function (stream) {
   stream = jsonStream(stream)
@@ -93,10 +137,24 @@ var server = net.createServer(function (stream) {
       return
     }
 
+    // encrypt log
+    var logBuf = Buffer.from(JSON.stringify(log))
+    var cipher = Buffer.alloc(logBuf.length + sodium.crypto_secretbox_MACBYTES)
+    var nonce = Buffer.alloc(sodium.crypto_secretbox_NONCEBYTES)
+    sodium.randombytes_buf(nonce)
+    sodium.crypto_secretbox_easy(cipher, logBuf, nonce, boxSecretKey)
+
+    var encryptedLog = {
+      cipher: cipher.toString('hex'),
+      nonce: nonce.toString('hex')
+    }
+
+    var plainText = Buffer.alloc(cipher.length - sodium.crypto_secretbox_MACBYTES)
+    sodium.crypto_secretbox_open_easy(plainText, cipher, nonce, boxSecretKey)
+
     // formats and writes entire log to disk on every transaction
     // this should be optimized in the future
-    var prettyLog = JSON.stringify(log, null, 2)
-    fs.writeFile('data.json', prettyLog, function (err) {
+    write('data.json', encryptedLog, function (err) {
       if (err) console.error(err)
     })
 
@@ -119,4 +177,11 @@ function sigToHex (message, secretKey) {
   var signature = Buffer.alloc(sodium.crypto_sign_BYTES)
   sodium.crypto_sign_detached(signature, message, secretKey)
   return signature.toString('hex')
+}
+
+function write (file, data, cb) {
+  fs.writeFile(file, JSON.stringify(data, null, 2), function (err) {
+    if (err) return cb(new Error('Could not write file ' + file))
+    cb()
+  })
 }
